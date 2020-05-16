37e757dd9b074c0998c763d73a4cc4a237fdad95
Arian <aryannamavar@gmail.com>
2020-05-16 11:17:58 -0700

    Initial Push

---
+++ app/HashMap.cpp
@@ -0,0 +1,175 @@
+// HashMap.cpp
+#include "HashMap.hpp"
+#include <iostream>
+#include <tuple>
+
+
+namespace
+{
+    unsigned int ascii_hash(const std::string& s)
+    {
+        int hash_value = 0;
+        for (int i=0; i<s.size();++i)
+        {
+            hash_value = 31*hash_value + s[i];
+        }
+        return hash_value;
+    
+    
+    }
+}
+
+HashMap::HashMap()
+    :hashFunction{ascii_hash}, sz{0}, cap{INITIAL_BUCKET_COUNT}, buckets{new Node*[INITIAL_BUCKET_COUNT]}
+{
+    for (unsigned int i=0;i<cap;++i)
+    {
+        buckets[i] = nullptr;
+    }
+    // std::cout << " Const " << hashFunction("Boo") << std::endl; 
+}
+
+
+HashMap::~HashMap()
+{
+    // delete[] buckets;
+    for (unsigned int i=0;i<cap;++i)
+    {
+        delete buckets[i];
+    }
+    delete[] buckets;
+}
+
+
+
+void HashMap::add(const std::string& key, const std::string& value)
+{
+    // unsigned int hash_val = hashFunction(key);
+    // unsigned int hash_bucket = hash_val % cap;
+    unsigned int hash_bucket = get_hash(key, cap);
+    std::cout << key << " Hash value is " << hash_bucket << std::endl;
+    // std::cout << hash_val << "   " << hash_bucket <<std::endl;
+    if (contains(key) == false)
+    {
+    // Node* ll = buckets[hash_bucket];
+
+    Node* new_node = new Node{key,value,nullptr};
+    buckets[hash_bucket] = new_node;
+    sz++;
+    }
+}
+
+
+// bool HashMap::remove(const std::string& key)
+// {
+//     if (contains(key) == true)
+//     {
+
+//     }
+//     return false;
+// }
+
+bool HashMap::contains(const std::string& key) const
+{
+    // unsigned int hash_val = hashFunction(key);
+    // unsigned int hash_bucket = hash_val % cap;
+    unsigned int hash_bucket = get_hash(key, cap);
+    // std::cout << " The hash bucket func right?  " << hash_bucket << std::endl;
+    // std::cout << hash_val << "   " << hash_bucket <<std::endl;
+    Node* ll = buckets[hash_bucket]; // buckets is **, an element of bucket is *
+    while (ll != nullptr)
+    {
+
+        if ((*ll).key == key)
+        {
+            return true;
+        }
+        ll = (*ll).next;
+    }
+    return false;
+    
+}
+
+
+std::string HashMap::value(const std::string& key) const
+{
+    
+    if (contains(key) == true)
+    {
+        unsigned int hash_bucket = get_hash(key,cap);
+        Node * ll = buckets[hash_bucket];
+        while (ll != nullptr)
+        {
+            if ((*ll).key == key )
+            {
+                return (*ll).value;
+            }
+        }
+    }
+    return "";
+}
+
+
+
+unsigned int HashMap::size() const
+{
+    return sz;
+}
+
+unsigned int HashMap::bucketCount() const
+{
+    unsigned int allocated_buckets = 0;
+    for (unsigned int i=0;i<cap;++i)
+    {
+        if(buckets[i] != nullptr)
+        {
+            allocated_buckets+= 1;
+        }
+    }
+    return allocated_buckets;
+}
+
+double HashMap::loadFactor() const
+{  
+    return double(sz)/double(cap);
+}
+
+unsigned int HashMap::maxBucketSize() const
+{
+    auto c = findLargestBucket();
+    return c;
+}
+
+unsigned int HashMap::findLargestBucket() const
+{
+    unsigned int current_max_index = 0; // find the index containing biggest bucket
+    unsigned int current_max_nodes = 0; // count the nodes the max index has
+    
+    unsigned int temp_max_nodes = 0; 
+    
+    for (unsigned int i=0; i<cap;++i)
+    {
+        Node* ll = buckets[i];
+        while (ll != nullptr)
+        {
+            temp_max_nodes+=1;
+            ll = (*ll).next;
+        }
+        if (temp_max_nodes >= current_max_nodes)
+        {
+            current_max_nodes = temp_max_nodes;
+            current_max_index = i;
+        }
+        temp_max_nodes = 0;
+    }
+    return current_max_nodes;
+}
+
+
+unsigned int HashMap::get_hash(const std::string& key, const unsigned int& capac) const
+    {
+        unsigned int hash_val = hashFunction(key);
+        // std::cout << hash_val << " from name spae " << std::endl;
+        unsigned int hash_bucket = hash_val % capac;
+        return hash_bucket;
+    }
---
+++ gtest/HashMapTests.cpp
@@ -0,0 +1,97 @@
+// HashMapTests.cpp
+//
+// ICS 45C Spring 2020
+// Project #3: Maps and Legends
+//
+// Write unit tests for your HashMap class here.  I've provided a few tests
+// already, though I've commented them out, because they won't compile and
+// link until you've implemented some things.
+//
+// Of course, you'll want to write lots more of these tests yourself, because
+// this is an inexpensive way to know whether things are working the way
+// you expect -- including scenarios that you won't happen to hit in the
+// course of testing your overall program.  (You might also want to be sure
+// that your HashMap implementation is complete and correct before you try
+// to write the rest of the program around it, anyway; there's a very
+// substantial amount of partial credit available if all that works is
+// HashMap.)
+
+#include <gtest/gtest.h>
+#include "HashMap.hpp"
+
+
+// TEST(HashMapTests, sizeOfNewlyCreatedHashMapIsZero)
+// {
+//    HashMap empty;
+//    ASSERT_EQ(0, empty.size());
+// }
+
+
+// TEST(HashMapTests, emptyMapContainsNoKeys)
+// {
+//    HashMap empty;
+//    ASSERT_FALSE(empty.contains("first"));
+//    ASSERT_FALSE(empty.contains("second"));
+//    ASSERT_FALSE(empty.contains(""));
+// }
+
+
+TEST(HashMapTests, containKeyAfterAddingIt)
+{
+   HashMap hm;
+   hm.add("Boo", "perfect");
+   hm.add("arian", "here");
+   hm.add("was", "123");
+   ASSERT_TRUE(hm.contains("Boo"));
+   ASSERT_TRUE(hm.contains("arian"));
+   ASSERT_TRUE(hm.contains("was"));
+   ASSERT_FALSE(hm.contains("NON_EXISTENT_KEY"));
+   ASSERT_EQ(3,hm.size());
+   
+
+   hm.add("Bool", "trues");
+   hm.add("dj", "khaled");
+   hm.add("daft", "punk");
+   ASSERT_TRUE(hm.contains("Bool"));
+   ASSERT_TRUE(hm.contains("dj"));
+   ASSERT_TRUE(hm.contains("daft"));
+   std::cout << "testing value " << std::endl;
+   ASSERT_EQ("trues", hm.value("Bool"));
+   ASSERT_EQ("khaled", hm.value("dj"));
+   ASSERT_EQ("punk", hm.value("daft"));
+   ASSERT_EQ("", hm.value("NON_EXISTENT_KEY2"));
+   // ASSERT_EQ(6,hm.size());
+
+}
+
+// TEST(HashMapTests, TestCopyCons)
+// {
+//     HashMap hm;
+//     hm.add("arian", "Here");
+//     hm.add("gar", "345");
+//     hm.add("terry", "678");
+
+//     HashMap hm2 = hm1;
+//     ASSERT_EQ(3, hm2.size());
+//     ASSERT_TRUE(hm2.contains("arian"));
+//     ASSERT_TRUE(hm2.contains("gar"));
+//     ASSERT_TRUE(hm2.contains("terry"));
+    
+// }
+
+
+// TEST(HashMapTests,  TestAssignmentCopy)
+// {
+//     HashMap hm, hm2;
+//     hm.add("arian", "123");
+//     hm.add("gar", "345");
+//     hm.add("terry", "678");
+
+//     hm2 = hm;
+//     ASSERT_EQ(3, hm2.size());
+//     ASSERT_TRUE(hm2.contains("arian"));
+//     ASSERT_TRUE(hm2.contains("gar"));
+//     ASSERT_TRUE(hm2.contains("terry"));
+
+// }
+
